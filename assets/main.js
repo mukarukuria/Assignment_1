79cc1b1296e9c696fbfcc1f452f520b40bcb6e26
mukarukuria <timothy.kuria@strathmore.edu>
2022-06-22 21:23:40 +0300

    first commit

---
+++ assets/main.js
@@ -0,0 +1,4 @@
+function displaytext() {
+    var name = document.getElementById("inputtext").value;
+    document.getElementById("paragraph").innerHTML = "Hello " + name +", welcome to the bootcamp"
+}
